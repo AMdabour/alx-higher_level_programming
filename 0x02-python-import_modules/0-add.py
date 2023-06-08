909c77aa10a3fab7485a88249b298d05ff2f1248
AMdabour <ah910905@gmail.com>
2023-06-08 02:17:25 -0700

    edit

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-a = 1
-b = 2
-
+# skip thhe code if it imported
 if __name__ == "__main__":
+    a = 1
+    b = 2
+
     from add_0 import add
     result = add(1, 2)
     print("{} + {} = {}".format(a, b, result))
