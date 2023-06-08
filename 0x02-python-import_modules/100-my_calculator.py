eb7c608b56ff1eb279dacd2a6359ffaea8f04510
AMdabour <ah910905@gmail.com>
2023-06-08 11:33:44 -0700

    calc

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
-    operators = ["+", "-", "*","/"]
+    operators = ["+", "-", "*", "/"]
     operator = argv[2]
 
     if operator not in operators:
