06807cc3636cbbfb248769b2bcd83fd501600dc0
AMdabour <ah910905@gmail.com>
2023-06-11 07:49:47 -0700

    matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    rows = len(matrix)
+    columns = len(matrix[0])
+    if matrix and matrix[0]:
+        for i in range(rows):
+            for j in range(columns):
+                if j < columns - 1:
+                    print("{:d}".format(matrix[i][j]), end=' ')
+                else:
+                    print("{:d}".format(matrix[i][j]), end='')
+            print()
+    else:
+        print()
