d9ee9311f4615369e25c4ca219d99a80d31a798c
AMdabour <ah910905@gmail.com>
2023-06-14 10:09:41 -0700

    edit

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(roman_string):
     from functools import reduce
     if roman_string and type(roman_string) is str:
         romnum = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
-                'C': 100, 'D': 500, 'M': 1000}
+                  'C': 100, 'D': 500, 'M': 1000}
         num = 0
         prev_value = 0
         for char in reversed(roman_string):
