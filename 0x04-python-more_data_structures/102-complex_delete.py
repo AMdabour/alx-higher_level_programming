ace62d1db42d7ea30c4bc30e20aa16e60a8afe9e
AMdabour <ah910905@gmail.com>
2023-06-14 13:08:45 -0700

    edit

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
+    rm_list = []
     for key, val in a_dictionary.items():
         if val == value:
-            a_dictionary.pop(key)
+            rm_list.append(key)
+    for key in rm_list:
+        a_dictionary.pop(key)
     return a_dictionary
