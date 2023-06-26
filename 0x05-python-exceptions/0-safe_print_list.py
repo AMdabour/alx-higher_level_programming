fadbbf94dde9711b039e6c8845fc76abaf87e842
AMdabour <ah910905@gmail.com>
2023-06-26 09:53:08 -0700

    trial edit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,12 +3,12 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     for i in my_list:
         try:
-            print("{}".format(i), end="")
+            print("{:d}".format(i), end="")
             count += 1
             if count == x:
                 break
-        except Exception:
-            pass
+        except TypeError:
+            break
     print()
     if x > count:
         return count
