8715f59689acf558a6a413688327cecec9e855f8
AMdabour <ah910905@gmail.com>
2023-06-26 05:18:24 -0700

    edit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -9,5 +9,5 @@ def safe_print_list(my_list=[], x=0):
             i += 1
         print()
         return i
-    except:
-        pass
+    except Exception as e:
+        print("An error occurred:", str(e))
