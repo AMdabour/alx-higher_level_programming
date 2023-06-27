6b928094ca60b2665303d91c84b63849db1d9d38
AMdabour <ah910905@gmail.com>
2023-06-27 14:17:34 -0700

    init

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""define a class Square"""
+
+
+class Square:
+
+    """
+    it is a class named Square contains information
+    about the size of the square
+    """
+
+    def __init__(self, size):
+
+        """
+        a method contains a private instance
+        variable of the size of the square
+        """
+
+        self._Square__size = size
