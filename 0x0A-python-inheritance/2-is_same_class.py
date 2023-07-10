f7f55274fbd9b6176192557efd3825b20d3a0420
a-dabour <ah910905@gmail.com>
2023-07-10 19:44:33 +0300

    is instance

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""define a module"""
+
+
+def is_same_class(obj, a_class):
+    """returns True if obj is instance of a_class else False"""
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
