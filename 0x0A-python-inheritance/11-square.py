62ebacb099a3dbcd094e7a07c4e858dc293da5c5
a-dabour <ah910905@gmail.com>
2023-07-10 14:10:28 -0700

    ...

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -16,4 +16,4 @@ class Square(Rectangle):
 
     def __str__(self):
         """returns string representation"""
-        return "[Square] " + str(self.__size) "/" + str(self.__size)
+        return "[Square] " + str(self.__size) + "/" + str(self.__size)
