94d1b5943ebff64ba5dafbe35d016f3124986185
a-dabour <ah910905@gmail.com>
2023-07-10 22:58:28 +0300

    inherit and validate

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""BaseGeometry module with BaseGeometry class"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """represents Rectangle"""
+    def __init__(self, width, height):
+        """initilize new instance with width and height"""
+        self.__width = width
+        self.__height = height
+        super().integer_validator("width", self.__width)
+        super().integer_validator("height", self.__height)
