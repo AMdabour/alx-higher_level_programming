6d85a40a3bcbe7b00ebaf8db74f25db0bd9c0188
a-dabour <ah910905@gmail.com>
2023-07-11 13:07:41 -0700

    edit

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
 """define a function to convert class into json representation"""
-import json
 
 
-def class_encoder(obj):
-    """encodes obj"""
+def class_to_json(obj):
+    """converts class into json format"""
     if isinstance(obj, MyClass):
         return {'name': obj.name, 'number': obj.number}
     return obj
-
-def class_to_json(obj):
-    """class to json"""
-    jsonrepr = hson.dumps(obj, default=class_encoder)
-    return jsonrepr
