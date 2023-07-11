9a8a934c61c8e098a6deadae39dce2d22d34e067
a-dabour <ah910905@gmail.com>
2023-07-11 03:58:08 -0700

    append

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+define a functions that appends a string to the end of a file
+and returns the nb_written
+"""
+
+
+def append_write(filename="", text=""):
+    """appends a string to  a file and returns nb_written"""
+    with open(filename, 'a', encoding="utf-8") as f:
+        nb_written = f.write(text)
