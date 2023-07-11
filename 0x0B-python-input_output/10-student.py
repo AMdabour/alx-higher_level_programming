dad2d16874bf6c273aea01103d4725419a3717f2
a-dabour <ah910905@gmail.com>
2023-07-11 13:41:09 -0700

    edit

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -6,18 +6,15 @@ class Student:
     """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
-        ""
-        "Initialize a new Studen
+        """
+        Initialize a new Studen
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """
-        Get a dictionary representation of the student
-        """
-        if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
+        """Get a dictionary representation of the student"""
+        if (type(attrs) == list and all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
