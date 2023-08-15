466184a619e7f588cd9c537af4c7f063c562b4f5
a-dabour <ah910905@gmail.com>
2023-08-16 00:35:00 +0300

    edit

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,3 @@
 -- convert to UTF8
-USE hbtn_0c_0;
-ALTER TABLE second_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `hbtn_0c_0`;
+ALTER TABLE `second_table` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
