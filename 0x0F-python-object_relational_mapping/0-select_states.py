c80f607fa9ee97e44c5cff36a0b8e9d5fc3cb614
a-dabour <ah910905@gmail.com>
2023-09-15 09:25:02 -0700

    select states table from a database

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,29 +1,18 @@
 #!/usr/bin/python3
 """a script that lists all states from the database hbtn_0e_0_usa"""
-
-
 import MySQLdb
 import sys
 
 
-mysql_username = sys.argv[1]
-mysql_password = sys.argv[2]
-database_name = sys.argv[3]
-
 if __name__ == "__main__":
-    try:
-        conn = MySQLdb.connect
-        (host="localhost", port=3306, user=mysql_username,
-            passwd=mysql_password, db=database_name)
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-        results = cur.fetchall()
-        for row in results:
-            print(row)
+    conn = MySQLdb.connect
+    (host="localhost", port=3306, user=sys.argv[1],
+        passwd=sys.argv[2], db=sys.argv[3])
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    results = cur.fetchall()
+    for row in results:
+        print(row)
 
-    except MySQLdb.Error as e:
-        print("MySQL Error:", e)
-
-    finally:
-        cur.close()
-        conn.close()
+    cur.close()
+    conn.close()
