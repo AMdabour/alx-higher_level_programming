5636c66a11c9cab192b072fc6808c2ff13f6c2fa
AMdabour <ah910905@gmail.com>
2023-09-29 07:36:52 -0700

    edit

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,13 +3,14 @@
 import urllib.request
 
 
-url = "https://alx-intranet.hbtn.io/status"
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
 
-with urllib.request.urlopen(url) as response:
-    content = response.read()
-    utf8_content = content.decode('utf-8')
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
+        utf8_content = content.decode('utf-8')
 
-    print("Body response:")
-    print(f"\t- type: {type(content)}")
-    print(f"\t- content: {content}")
-    print(f"\t- utf8 content: {utf8_content}")
+        print("Body response:")
+        print(f"\t- type: {type(content)}")
+        print(f"\t- content: {content}")
+        print(f"\t- utf8 content: {utf8_content}")
