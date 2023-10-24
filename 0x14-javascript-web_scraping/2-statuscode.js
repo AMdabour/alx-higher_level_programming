4857d0d9bbafcd5a6fc009d5427dc6d91f796bd6
a-dabour <ah910905@gmail.com>
2023-10-24 19:12:46 +0300

    edit

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,6 +5,6 @@ request.get(process.argv[2], function (error, response) {
   if (error) {
     console.error(error);
   } else {
-    console.log('code: ', response.statusCode);
+    console.log('code: ', response && response.statusCode);
   }
 });
