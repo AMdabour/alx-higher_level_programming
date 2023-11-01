72cfeac8708e666c69747050b457a6afab005f1b
a-dabour <ah910905@gmail.com>
2023-11-01 17:11:34 +0200

    added some jquery scripts

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,12 @@
+$(function ()
+{
+    $('DIV#toggle_header').click(function ()
+    {
+        const header = $('header');
+        if (header.hasClass('red')) {
+            header.removeClass('red').addClass('green');
+        } else if (header.hasClass('green')) {
+            header.removeClass('green').addClass('red')
+        }
+    });
+});
